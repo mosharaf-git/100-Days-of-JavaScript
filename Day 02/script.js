6421dc661d5de2827a9a64915cd4570739ace77b
mosharaf-h <mosharaf.dev@gmail.com>
2021-10-02 11:31:54 -0700

    Day 02

--- Day 02/script.js
+++ Day 02/script.js
@@ -1,13 +1,14 @@
 var button = document.querySelector('button');
+var h1 = document.querySelector('h1');
 var body = document.querySelector('body');
-var colors = ['green', 'yellow', 'orange', 'purple', 'pink'];
+var colors = ['#FF5733', '#DFFF00', '#FFBF00', '#FF7F50', '#DE3163', '#9FE2BF', '#40E0D0','#6495ED', '#CCCCFF'];
 
-body.style.backgroundColor = '#fff';
+body.style.backgroundColor = '#ffff';
 button.addEventListener('click', changeBackground);
 
 function changeBackground(){
     var colorIndex = parseInt(Math.random()*colors.length);
     body.style.backgroundColor = colors[colorIndex];
-};
-
-
+    h1.innerHTML = colors[colorIndex];
+    h1.style.color = colors[colorIndex];
+}
